ea1d1ad57ddba3022a9873a06855acbbb6c6b887
AndewNN <p50629.2013x@gmail.com>
2025-09-16 21:56:25 +0700

    merge .sh and minor changes

--- CUDA/run_merge.sh
+++ CUDA/run_merge.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate cudaq
+
+# Usage: ./run_merge.sh Q A B
+# Example: ./run_merge.sh 4 3 3
+
+# source ~/miniconda3/etc/profile.d/conda.sh
+# conda activate cudaq
+
+Q=$1
+A=$2
+B=$3
 
 # Copy X from Xbase
-python PO_Mixer_Benchmark.py -Q 4 -A 3 -B 3 -st 0 -ed 25 -m Preserving
-python PO_Mixer_Benchmark.py -Q 4 -A 3 -B 3 -st 25 -ed 50 -m Preserving
-python PO_Mixer_Benchmark.py -Q 4 -A 3 -B 3 -st 50 -ed 75 -m Preserving
-python PO_Mixer_Benchmark.py -Q 4 -A 3 -B 3 -st 75 -ed 100 -m Preserving
+python PO_Mixer_Benchmark.py -Q $Q -A $A -B $B -st 0  -ed 25  -m Preserving
+python PO_Mixer_Benchmark.py -Q $Q -A $A -B $B -st 25 -ed 50  -m Preserving
+python PO_Mixer_Benchmark.py -Q $Q -A $A -B $B -st 50 -ed 75  -m Preserving
+python PO_Mixer_Benchmark.py -Q $Q -A $A -B $B -st 75 -ed 100 -m Preserving
 
-# Merge from splits
-python PO_Mixer_Benchmark_Merger.py -Q 4 -A 3 -B 3
-# Generate report
-python PO_Mixer_Benchmark.py -Q 4 -A 3 -B 3
+# Merge from splits and Generate report
+python PO_Mixer_Benchmark_Merger.py -Q $Q -A $A -B $B
 
