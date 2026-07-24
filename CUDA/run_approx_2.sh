6aeaea0aa6516d8bbcca821eaf7f9a815c164a69
AndewNN <p50629.2013x@gmail.com>
2026-07-25 00:51:27 +0700

    run approx ratio

--- CUDA/run_approx_2.sh
+++ CUDA/run_approx_2.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-# python PO_new_ApproxRatio.py -Q 2 -A 3 4 5 6 7 -E 35 -p 5 -B 24 -m Preserving -L 0.00005 -q 0.15
-# python PO_new_ApproxRatio.py -Q 2 -A 10 -E 35 -p 5 -B 24 -m Preserving -L 0.00005 -q 0.15
-
-# python PO_new_ApproxRatio.py -Q 2 -A 3 4 5 6 7 -E 35 -p 5 -m X -L 0.05 -q 0.15
-# python PO_new_ApproxRatio.py -Q 2 -A 3 4 5 6 7 -E 35 -p 5 -m X -L 0.005 -q 0.15
-
-python PO_new_ApproxRatio.py -Q 2 -A 3 4 5 6 7 -E 35 -p 5 -m X -L 0.05 -q 0.15 -b_X 250
+for assets in 3 4 5 6 7 8
+do
+    for lambda in 0.0005 0.005 0.05 0.5 5
+    do
+        for layer in 5 7 9
+        do
+            python PO_new_ApproxRatio.py -Q 2 -A $assets -q 1.5 -m X -L $lambda -b_X 0 -E 10 -p $layer -norm Jh -d_b 0.3 -d_b 0.6 --LR_init
+        done
+    done
+done
+                
+# for assets in 7
+for assets in 3 4 5 6 7
+do
+    for layer in 5 7 9
+    do
+        python PO_new_ApproxRatio.py -Q 2 -A $assets -q 1.5 -m Preserving -L 1 -b_P 0 -E 10 -p $layer -norm Jh -d_b 0.3 -d_b 0.6 --LR_init
+    done
+done
